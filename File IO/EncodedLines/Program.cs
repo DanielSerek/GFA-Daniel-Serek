1aaba590f40f4dc9df20e195839fbcb0603c5722
DanielSerek <daniel.serek.uk@gmail.com>
2020-03-18 19:38:19 +0100

    Lottery

---
+++ File IO/EncodedLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace EncodedLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] input = File.ReadAllLines(@"encoded-lines.txt");
+                string[] output = new string[input.Length];
+                char ch;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        ch = Convert.ToChar(input[i][j]);
+                        if (ch == 32) output[i] += ch;
+                        else
+                        {
+                            ch--;
+                            output[i] += ch;
+                        }
+                    }
+                    output[i] += Environment.NewLine;
+                }
+                string outputString = String.Concat(output); ;
+                Console.WriteLine(outputString);
+                File.WriteAllText(@"../../../output.txt", outputString); ;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
